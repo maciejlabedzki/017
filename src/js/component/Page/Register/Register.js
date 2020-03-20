ab09698f860b510fb35e8973e64c23b67e6dd0ac
MaciejLabedzki <maciejlabedzki@gmail.com>
2020-03-20 14:08:10 +0100

    update

--- src/js/component/Page/Register/Register.js
+++ src/js/component/Page/Register/Register.js
@@ -6,6 +6,9 @@ class Register extends React.Component {
     super(props);
     this.state = {
       message: "",
+      removeUserID: undefined,
+      removed: false,
+      removedError: undefined,
       user: {
         key: Date.now(),
         name: "",
@@ -67,52 +70,60 @@ class Register extends React.Component {
     };
 
     this.setState({ user });
-    // console.log(form, data, data.get("name"));
-    // for (let name of data.keys()) {
-    //   console.log(data.get(name));
-    //   //this.setState({ name: })
-    //   // const input = form.elements[name];
-    //   // const parserName = input.dataset.parse;
-
-    //   // if (parserName) {
-    //   //   const parser = inputParsers[parserName];
-    //   //   const parsedValue = parser(data.get(name));
-    //   //   data.set(name, );
-    //   // }
-    // }
-    // console.log("name", data);
   };
 
-  updateInputs = e => {
-    this.setState({});
+  jsonApiDel = async id => {
+    // Information
+    // {
+    //   "user": {
+    //     "key": 1584700360498,
+    //     "name": "3",
+    //     "lastName": "4",
+    //     "mail": "bedzki@wp.pl",
+    //     "password": "a"
+    //   },
+    //   "id": 3 // <= THIS NUMBER IS FOR URL .../user/3
+    // },
+    if (this.state.removeUserID !== "") {
+      await axios
+        .delete("http://localhost:3333/user/" + this.state.removeUserID)
+        .then(response => {
+          this.setState({ removed: true, removedError: undefined });
+        })
+        .catch(error => {
+          console.log(error.toJSON());
+          let errorStorage = error.toJSON();
+          this.setState({
+            removedError: errorStorage["message"]
+          });
+        });
+    } else {
+      console.log("No user id");
+    }
   };
 
-  jsonApiDel = async () => {
-    await axios.delete("http://localhost:3333/user/a1");
+  setRemoveUserID = e => {
+    e.preventDefault();
+    let form = e.target;
+    let data = new FormData(form);
+    this.setState({ removeUserID: data.get("removeID") }, () => {
+      this.jsonApiDel();
+    });
   };
 
-  componentDidMount() {}
-
-  show = () => {
+  showState = () => {
     console.log(this.state);
-    console.log(global);
   };
 
   render() {
     return (
       <React.Fragment>
         <div className="app_container">
-          Register
-          <button onClick={this.show}>Test</button>
-          <form onSubmit={this.handleSubmit}>
+          Register <button onClick={this.showState}>Show State</button>
+          <form className="app_form" onSubmit={this.handleSubmit}>
             <div className="input_container">
               <label>Name:</label>
-              <input
-                name="name"
-                placeholder="Name"
-                onChange={this.updateInputs}
-                required
-              ></input>
+              <input name="name" placeholder="Name" required></input>
             </div>
             <div className="input_container">
               <label>Last Name:</label>
@@ -149,13 +160,33 @@ class Register extends React.Component {
           <p>
             <button onClick={this.jsonApiPost}>post</button>
           </p>
-          <p>
-            <button onClick={this.jsonApiDel}>del</button>
-          </p>
+          <p></p>
           <p>
             <button onClick={this.jsonApiPut}>put</button>
           </p>
           <div className="form-message">{this.state.message}</div>
+          <form onSubmit={this.setRemoveUserID}>
+            Remove User ID:
+            {this.state.removeUserID === undefined && (
+              <div className="alert-warning">No user ID</div>
+            )}
+            {this.state.removeUserID !== undefined && (
+              <div className="alert-success">
+                User ID: {this.state.removeUserID}
+              </div>
+            )}
+            <input name="removeID" placeholder="User ID to remove" />
+            <input type="submit" value="Submit" />
+            {this.state.removed === true && (
+              <div className="alert-success">
+                User ID: {this.state.removeUserID} was deleted
+              </div>
+            )}
+            {this.state.removedError !== undefined && (
+              <div className="alert-warning">{this.state.removedError}</div>
+            )}
+          </form>
+          {/* <button onClick={this.jsonApiDel}>Delete </button> */}
         </div>
       </React.Fragment>
     );
