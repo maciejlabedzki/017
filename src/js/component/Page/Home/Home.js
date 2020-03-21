113fa0de60b3cf4dc0c0c74c7dbf4088e91230cd
MaciejLabedzki <maciejlabedzki@gmail.com>
2020-03-21 15:37:04 +0100

    update

--- src/js/component/Page/Home/Home.js
+++ src/js/component/Page/Home/Home.js
@@ -5,7 +5,23 @@ const Home = props => {
     <React.Fragment>
       <div className="app_container app_home">
         <label className="icon-home">Home</label>
-        <div></div>
+        <ul>
+          <li>Search movies</li>
+          <li>
+            User Account [ login , password ]
+            <ul>
+              <li>Favourite</li>
+            </ul>
+          </li>
+          <li>
+            Admin Account
+            <ul>
+              <li>delete users</li>
+              <li>inspect all users</li>
+            </ul>
+          </li>
+          <li></li>
+        </ul>
       </div>
     </React.Fragment>
   );
